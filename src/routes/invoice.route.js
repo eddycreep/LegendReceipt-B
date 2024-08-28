4ac6ef51aa799314a3def345fab516b2fe2bc0b4
Yanga <yangagovuza@gmail.com>
2024-08-28 12:16:02 +0200

    testing

--- src/routes/invoice.route.js
+++ src/routes/invoice.route.js
@@ -5,6 +5,6 @@ const InvoiceController = require('../controllers/invoice.controller');
 
 require('dotenv').config({ path: './configuration.env' });
 
-router.get('/getreceiptdata/:docnum', InvoiceController.getReceiptData); //test
+router.get('/getreceiptdata/:docnum', InvoiceController.getReceiptData); //test - testing
 
 module.exports = router;
