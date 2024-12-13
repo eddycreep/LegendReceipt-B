2f7a4eefce8d18da2b5d72f67b7e7df858926e79
yanga17 <yangagovuza@gmail.com>
2024-12-13 12:17:28 +0200

    test

--- config/db.config.js
+++ config/db.config.js
@@ -1,19 +1,17 @@
-const mysql = require('mysql2'); // Updated to use 'mysql2' package for better performance and promise support
+const mysql = require('mysql'); // Updated to use 'mysql2' package for better performance and promise support
 require('dotenv').config({ path: './configuration.env' });
 
 // Create a pool of connections
 const dbConn = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
+    host: process.env.HOST,             
+    port: process.env.PORT,        
+    user: process.env.USER,        
+    password: process.env.PASSWORD,    
     database: process.env.DATABASE,
-    waitForConnections: true,
-    connectionLimit: 1, // Reduce connection limit to 1 for serverless functions
-    queueLimit: 0,
-    ssl: {
-        rejectUnauthorized: false
-    },
-    connectTimeout: 120000, // Set a reasonable connection timeout (in ms)
+    waitForConnections: true,           // Enable connection queuing when the pool is exhausted
+    connectionLimit: 1,                // Limit the number of simultaneous connections in the pool
+    queueLimit: 0,                      // Unlimited request queuing
+    connectTimeout: 120000,              // Timeout for establishing a new connection (10 seconds)
 });
 
 // Test the database connection
